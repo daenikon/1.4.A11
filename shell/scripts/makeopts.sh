46aebcecd6932669c1de31ff51de3e0338355791
den <dkondratiuk06@gmail.com>
2023-08-16 16:15:13 +0200

    Update makeopts.sh

--- shell/scripts/makeopts.sh
+++ shell/scripts/makeopts.sh
@@ -2,5 +2,5 @@
 
 # RAM/2GB
 ram_gb=$(free -g | awk '/Mem:/ {print int($2/2 + 0.5)}')
-makeopts_jobs=$(($ram_gb < $(nproc) ? $ram_gb : $(nproc)))
-echo 'MAKEOPTS="-j$makeopts_jobs -l$makeopts_jobs"'
+jobs=$(($ram_gb < $(nproc) ? $ram_gb : $(nproc)))
+echo 'MAKEOPTS="-j$jobs -l$jobs"'
